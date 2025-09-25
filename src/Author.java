01ff8873ef068ea2a517b1b03f4d187388ebffeb
StanislavKlyuev <patriek@rambler.ru>
2025-09-25 18:59:39 +0300

    Initial Commit

--- src/Author.java
+++ src/Author.java
@@ -7,16 +7,8 @@ public class Author {
         this.surname = surname;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
     }
 
     public String getSurname() {
