be46f17936aa14a4846513d24090c18a0e9911a9
StanislavKlyuev <patriek@rambler.ru>
2025-09-25 19:02:21 +0300

    HW-9

--- src/Author.java
+++ src/Author.java
@@ -7,8 +7,16 @@ public class Author {
         this.surname = surname;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getSurname() {
--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,7 @@
 public class Book {
     private String title;
     private Author author;
-    private int publicationYear;
+    int publicationYear;
 
     public Book(String title, Author author, int publicationYear) {
         this.title = title;
@@ -9,8 +9,16 @@ public class Book {
         this.publicationYear = publicationYear;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
     }
 
     public Author getAuthor() {
